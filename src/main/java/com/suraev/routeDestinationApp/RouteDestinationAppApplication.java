e026fd7e9e77f77ddcd0dc388bc5fc42d3de33c7
sh4dowbolt <dmit.palckin@yandex.ru>
2025-06-04 18:16:31 +0400

    first commit

---
+++ src/main/java/com/suraev/routeDestinationApp/RouteDestinationAppApplication.java
@@ -0,0 +1,13 @@
+package com.suraev.routeDestinationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteDestinationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteDestinationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suraev/routeDestinationApp/RouteDestinationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraev.routeDestinationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteDestinationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
